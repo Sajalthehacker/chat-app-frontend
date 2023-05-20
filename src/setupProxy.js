f810777c4b9035c9f2880f694a9250ec103fc0f4
Sajalthehacker <97801174+Sajalthehacker@users.noreply.github.com>
2023-05-20 17:59:58 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "chat-app-backend-wheat.vercel.app",
       changeOrigin: true,
     })
   );
